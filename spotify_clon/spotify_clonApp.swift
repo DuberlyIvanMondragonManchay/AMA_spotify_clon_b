b7f42610e69d7b42af97ca233d18f1afa4a5ae1b
Configura tu nombre <Configura tu email>
2024-05-11 15:11:46 -0500

    Initial Commit

---
+++ spotify_clon/spotify_clonApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_clonApp.swift
+//  spotify_clon
+//
+//  Created by Mac8 on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct spotify_clonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
